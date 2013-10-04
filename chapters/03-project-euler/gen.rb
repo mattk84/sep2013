e0c6e98bd35b134a647039a0f635c1bf9666c8e9
Lee Green <lee.x.green@gmail.com>
2013-10-04 00:23:27 -0400

    a script to generate a project Euler template

---
+++ chapters/03-project-euler/gen.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+raise "I need an argument -- problem number" if ARGV.size == 0
+
+problem_number = ARGV[0] # 3
+formatted = '%03d' % problem_number # '003'
+filename = "#{formatted}.rb" # '003.rb'
+
+raise "#{filename} exists!" if File.exist?(filename)
+
+File.open(filename, 'w+') do |f|
+    f.write <<"EOS"
+#!/usr/bin/env ruby
+
+class Problem#{num}
+    def self.run
+    end
+end
+
+p Problem#{num}.run
+EOS
+
+end
